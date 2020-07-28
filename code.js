3b55e9d6603eb89144ef551b3f6721ba3f8dd0ad
Dani Roxberry <dani@bitoriented.com>
2020-07-28 15:24:40 -0700

    [fix] timeout bug

--- code.js
+++ code.js
@@ -1,59 +1,34 @@
 const kb = require("ble_hid_keyboard");
 NRF.setServices(undefined, { hid: kb.report });
 
-// Modify this value to change the timeout for LEDs.
-const TIMEOUT_SECONDS = 30;
+// Configurable settings:
+const LED_TIMEOUT = 600000;           // 10 minutes.
+let isMuted = true;                   // Assume we start muted.
 
-// Set a consistent timeout in milliseconds.
-const ledTimeout = TIMEOUT_SECONDS * 60 * 1000;
-
-// Assume we start muted.
-let isMuted = true;
-
-// resetLEDs resets all three LEDs.
-function resetLEDs() {
-    LED1.reset();
-    LED2.reset();
-    LED3.reset();
-}
-
-// setMute toggles the status indication LEDs.
-// Green for unmuted, red for muted.
+// Toggle the status indication LEDs. Green = unmuted, red = muted.
 function setMute(muted) {
     isMuted = muted;
-    resetLEDs();
-
-    if (isMuted) {
-        LED1.set();
-        setTimeout("LED1.reset()", ledTimeout);
-    }
-    else {
-        LED2.set();
-        setTimeout("LED2.reset()", ledTimeout);
-    }
+    if (isMuted) { LED1.set(); } else { LED2.set(); }
+    setTimeout(isMuted ? "LED1.reset()" : "LED2.reset()", LED_TIMEOUT);
 }
 
-// toggleMute sends CTRL + ALT + M, mapped to a script that toggles mute for all devices.
-function toggleMute() {
+// Mute te all devices once connected.
+kb.tap(kb.KEY.X, kb.MODIFY.LEFT_CTRL | kb.MODIFY.LEFT_ALT, function () {
+    setMute(true);
+});
+
+// Pressing the button sends CTRL + ALT + M, mapped to a script that toggles mute for all devices.
+setWatch(function () {
     kb.tap(kb.KEY.M, kb.MODIFY.LEFT_CTRL | kb.MODIFY.LEFT_ALT, function () {
+        LED1.reset();
+        LED2.reset();
         setMute(!isMuted);
     });
-}
+}, BTN, { edge: "rising", debounce: 10, repeat: true });
 
-// checkBattery notifies the user with a blue LED flash if the battery is less than or equal to 10%.
-function checkBattery() {
-    const batteryLevel = E.getBattery();
-    if (batteryLevel<= 10) {
-        resetLEDs();
-        digitalWrite(LED3, 1, 200);
-    }
-    console.log("Battery:\t", batteryLevel);
-}
+NRF.on('disconnect', function () {
+    LED1.reset();
+    LED2.reset();
+});
 
-// Watch for button presses.
-setWatch(function () { toggleMute(); }, BTN, { edge: "rising", debounce: 25, repeat: true });
-
-// Once connected, reset all LEDs, check the battery level, then unmute all devices.
-resetLEDs();
-checkBattery();
-setMute(true);
+NRF.setAdvertising({}, { name: "Mute Button", interval: 600 });
